bba5c8548e85b1ba2c076aebe00eee4288e5399f
Amit <amitrajpant7@gmail.com>
2024-01-13 14:37:22 +0545

    Script Ready compilation Done

--- blocks_quantization_vitis_ai_3.0/qt_and_compile.sh
+++ blocks_quantization_vitis_ai_3.0/qt_and_compile.sh
@@ -1,31 +1,40 @@
 #!/bin/bash
-
+#cleaning up 
+sudo rm -r ./compiled_all_blocks/*
+counter=0
 
 for file in ./float/*
 do
-if [ -f "$file" ]; then
-    echo ------------------------starting quantization-------------------- 
+if [ -f "$file" ]; then 
     name=$(echo "$file" | sed 's|./float/||') 
-    in_shape=$(echo $file | grep -o 'in_.*-out' | sed 's/in_//;s/-out//')
+    in_shape=$(echo "$name" | grep -o 'in_.*-out' | sed 's/in_//;s/-out//')
     IFS='x'
    
+    echo -----------------------Strating Qt $name -----------------------------
+
     #convert to list[]
     read -ra input_shape<<< $in_shape
     input=$(echo [${input_shape[0]}, ${input_shape[1]}, ${input_shape[2]}])
 
     #  To quantize certain block of model 
-    sudo /opt/vitis_ai/conda/envs/vitis-ai-pytorch/bin/python code/quantization.py --device "cpu" --quant_mode calib --subset_len 10 --model_name "$name"  --input_shape $input
+    sudo /opt/vitis_ai/conda/envs/vitis-ai-pytorch/bin/python code/quantization.py --device "cpu" --quant_mode calib --subset_len 100 --model_name "$name"  --input_shape $input
     echo Compilation
     sudo /opt/vitis_ai/conda/envs/vitis-ai-pytorch/bin/python ./code/quantization.py --quant_mode test --subset_len 1 --batch_size=1 --deploy --model_name "$name" --input_shape $input
 
-    # To deploy model and get the qunatized model in (onnx, pth, xmodel formta)
-    sudo -E PATH=$PATH:/opt/vitis_ai/conda/envs/vitis-ai-pytorch/bin /opt/vitis_ai/conda/envs/vitis-ai-pytorch/bin/vai_c_xir -x ./quantize_result/*.xmodel -a arch.json -o ./compiled -n "$name"
-
-    mv compiled_model/*.xmodel ./compiled_all_blocks/
-    rm -r compiled_model/*
-    rm -r quantized/*
-    rm -r quantize_result/*
-   break
-
+    deploy_name=$(echo "$name" |sed 's/.pth//')
+      
+# To deploy model and get the qunatized model in (onnx, pth, xmodel formta)
+    sudo -E PATH=$PATH:/opt/vitis_ai/conda/envs/vitis-ai-pytorch/bin /opt/vitis_ai/conda/envs/vitis-ai-pytorch/bin/vai_c_xir -x ./quantize_result/*.xmodel -a arch.json -o ./compiled -n "$deploy_name"
+    
+   sudo mkdir ./compiled_all_blocks/"$deploy_name"
+   sudo mv ./compiled/* ./compiled_all_blocks/"$deploy_name"
+   #sudo rm -r ./compiled/*
+   sudo rm -r ./quantized/*
+   sudo rm -r ./quantize_result/*
+  
+   ((counter++))
+   echo -e "\n\n"
+   echo -e "---------------------------Done Block $counter-------------------------\n\n"
+  
 fi
 done
